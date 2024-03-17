918d573b7cfea008a81e28758281ef12096a4007
Habiba <160347457+Habiba-Wasim@users.noreply.github.com>
2024-03-17 22:32:37 +0500

    Step 21 of Typescript
    Step 21 of Typescript

---
+++ Q21.js
@@ -0,0 +1,4 @@
+"use strict";
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let person = { name: "Habiba", fname: "Wasim", age: 20 };
+console.log(person);
---
+++ Q21.ts
@@ -0,0 +1,4 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person:{name:string, fname:string, age:number} ={name: "Habiba", fname: "Wasim", age:20}
+console.log(person)
